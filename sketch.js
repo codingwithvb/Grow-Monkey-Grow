60ee58d0984e476d64134c4de772439b6df71874
codingwithvb <72670340+codingwithvb@users.noreply.github.com>
2021-03-15 16:10:09 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var monkey, monkeyAnimation;
 var obstacle, obstacleImage;
 var ground, groundImage;
+var ground2, ground2Image; 
 var banana, bananaImage;
-var bananaGroup, obstacleGroupl, banana1Group;
+var bananaGroup, obstacleGroupl, banana1Group, obstacle1Group;
 var infiniteMode, infiniteModeImage; 
 var attackMode, attackModeImage; 
 var timedGame, timedGameImage; 
@@ -11,7 +12,8 @@ var batterylife, batterylifeImage;
 var battery1, battery1Image;
 var battery2, battery2Image
 var batteryFull, batteryFullImage;
-var batteryEmpty, batteryEmptyImage;  
+var batteryEmpty, batteryEmptyImage;
+var Multiplayer2, Multiplayer2Image;   
 var INFINITE = 0;
 var STONEATTACK = 1; 
 var HOME = 2; 
@@ -22,12 +24,17 @@ var TIMERINTRODUCTION = 6;
 var TIMER = 7;
 var congratsMessage = 8; 
 var INTRO = 9;  
+var MULTIPLAYER = 10; 
+var MULTIPLAYER_OVER = 11; 
 var gameState = INTRO; 
 var homeMonkey, homeMonkeyAnimation;
+var monkey2, monkey2Animation; 
 var homePage, homePageImage; 
 var score;
+var score2; 
 var timer1;
 var timer2;
+var timer3; 
 var threestones, threestonesImage;
 var obstacleHit; 
 
@@ -47,6 +54,9 @@ function preload(){
   batteryFullImage = loadImage("BatteryFull.png");
   timedGameImage = loadImage("timedGame.png");
   homePageImage = loadImage("homepageImage.png");
+  ground2Image = loadImage("Multiplayer.png");
+  Multiplayer2Image = loadImage("Multiplayer2.png");
+  monkey2Animation = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
   homeMonkeyAnimation = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
 }
 
@@ -57,6 +67,11 @@ function setup() {
   ground.scale = 0.55;
   ground.addImage(groundImage);
   ground.visible = false;
+
+  ground2 = createSprite(200,240,400,20);
+  ground2.scale = 1.1; 
+  ground2.addImage(ground2Image);
+  ground2.visible = false; 
   
   monkey = createSprite(100,335,20,50);
   monkey.addAnimation("monkey", monkeyAnimation);
@@ -68,6 +83,12 @@ function setup() {
   homeMonkey.addAnimation("homepage",homeMonkeyAnimation);
   homeMonkey.scale = 0.1;
   homeMonkey.visible = false;
+  
+  monkey2 = createSprite(100,135,20,50);
+  monkey2.addAnimation("MULTIPLAYER", monkey2Animation); 
+  monkey2.scale = 0.08; 
+  monkey2.visible = false;
+  monkey2.setCollider("circle",20,20,60); 
 
   homePage = createSprite(225,75,50,100);
   homePage.addImage(homePageImage);
@@ -124,19 +145,29 @@ function setup() {
   timedGame.scale = 0.2; 
   timedGame.visible = false; 
 
+  Multiplayer2 = createSprite(300,200,100,50);
+  Multiplayer2.addImage(Multiplayer2Image);
+  Multiplayer2.scale = 0.2; 
+  Multiplayer2.visible = false; 
+
   invisibleGround = createSprite(200,345 ,400,10);
   invisibleGround.visible = false; 
+
+  invisibleGround2 = createSprite(200,155,400,10);
+  invisibleGround2.visible = false;
   
   bananaGroup = new Group();
   obstacleGroup = new Group();
   monkeyGroup = new Group();
   banana1Group = new Group(); 
+  obstacle1Group = new Group(); 
   
   score = 0;
+  score2 = 0; 
   obstacleHit = 0;
-  timer1 = 6; 
+  timer1 = 5; 
   timer2 = 181;   
-
+  timer3 = 60;
 }
 
 function draw() {
@@ -172,23 +203,29 @@ function draw() {
     batterylife.visible = true;
     timedGame.visible = true;
     homePage.visible = true; 
+    Multiplayer2.visible = true; 
 
     monkey.visible = false;
     ground.visible = false; 
     spaceBar.visible = false; 
+    monkey2.visible = false; 
 
     homePage.x = 225; 
     homePage.y = 75; 
 
     score = 0;
-    timer1 = 6;
+    score2 = 0;
+    timer1 = 5;
     timer2 = 181;
+    timer3 = 60;
+    timer4 = 60;  
     obstacleHit = 0;
 
   obstacleGroup.setLifetimeEach(0);
   bananaGroup.setLifetimeEach(0);
 
     monkey.collide(invisibleGround);
+    monkey2.collide(invisibleGround2);
 
     monkey.scale = 0.08; 
 
@@ -207,6 +244,9 @@ function draw() {
     if(mousePressedOver(timedGame)){
       gameState = TIMERINTRODUCTION; 
     }
+    if(mousePressedOver(Multiplayer2)){
+      gameState = MULTIPLAYER;
+    }
   }
 
   if(gameState === INFINITE){
@@ -219,6 +259,8 @@ function draw() {
     batterylife.visible = false; 
     timedGame.visible = false; 
     homePage.visible = false; 
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
 
     monkey.visible = true; 
     ground.visible = true;
@@ -295,6 +337,8 @@ if(score >= 60){
     batterylife.visible = false;
     timedGame.visible = false; 
     homePage.visible = false; 
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
 
     monkey.visible = true; 
     ground.visible = true;
@@ -363,6 +407,8 @@ if(score >= 60){
     batterylife.visible = false;
     timedGame.visible = false; 
     homePage.visible = false; 
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
     
     monkey.visible = true; 
     ground.visible = true;
@@ -447,6 +493,8 @@ if(score >= 60){
     batterylife.visible = false;
     timedGame.visible = false; 
     homePage.visible = false; 
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
 
     monkey.visible = true; 
     ground.visible = true;
@@ -540,6 +588,8 @@ if(score >= 60){
     threestones.visible = false;
     batterylife.visible = false;
     timedGame.visible = false;
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
 
     homePage.visible = true; 
 
@@ -570,6 +620,8 @@ if(score >= 60){
     threestones.visible = false;
     batterylife.visible = false; 
     homePage.visible = false; 
+    monkey2.visible = false; 
+    Multiplayer2.visible = false; 
 
     monkey.visible = true; 
     ground.visible = true;
@@ -654,6 +706,184 @@ if(score >= 60){
 }
   }
 
+  if(gameState === MULTIPLAYER){
+
+    attackMode.visible = false;
+    infiniteMode.visible = false; 
+    homeMonkey.visible = false; 
+    threestones.visible = false;
+    batterylife.visible = false; 
+    timedGame.visible = false; 
+    homePage.visible = false; 
+    ground.visible = false; 
+    Multiplayer2.visible = false; 
+
+    monkey.visible = true; 
+    ground2.visible = true;
+    spaceBar.visible = true;
+    monkey2.visible = true; 
+
+    spawnFood();
+    spawnObstacles();
+    spawnFood2();
+    spawnObstacles2(); 
+  
+    if(keyDown("space")&& monkey.y >= 332 ) {
+      monkey.velocityY = -13.5;
+    }
+  
+    if(mousePressedOver(spaceBar) && monkey.y >= 332){
+      monkey.velocityY = -13.5;
+    }
+
+    monkey.velocityY = monkey.velocityY + 0.8 
+  
+    monkey.collide(invisibleGround);
+
+    if(keyDown("w")&& monkey2.y >= 120 ) {
+      monkey2.velocityY = -13.5;
+    }
+  
+    if(keyDown("UP_ARROW") && monkey2.y >= 120){
+      monkey2.velocityY = -13.5;
+    }
+
+    monkey2.velocityY = monkey2.velocityY + 0.8 
+  
+    monkey2.collide(invisibleGround2);
+
+    
+    stroke("black");
+    fill("black");
+    textFont("Arial");
+    textSize(25);
+    text(timer3, 200,445);
+    
+    if(obstacleGroup.isTouching(monkey)){
+      monkey.scale = 0.08; 
+      score = 0; 
+    }
+
+    if(obstacle1Group.isTouching(monkey2)){
+      monkey2.scale = 0.08; 
+     score2 = 0; 
+    }
+
+    if(bananaGroup.isTouching(monkey)){
+      score = score + 2;
+      bananaGroup.destroyEach();
+    }
+
+    if(banana1Group.isTouching(monkey2)){
+      score2 = score2 + 2;
+      banana1Group.destroyEach();
+    }
+ 
+
+    stroke("black");
+    textSize(20);
+    fill("black");
+    text(score, 10,435);
+
+    if(score <10){
+      text("vs. " + score2, 30,435)
+    }
+    if(score >= 10){
+      text("vs. " + score2, 40,435)
+    }
+
+    stroke("black");
+    textSize(15);
+    fill("brown")
+    text("GrowMonkeyGrow: Multiplayer (BETA)", 115,420);
+  
+    if (frameCount % 30 == 0 && timer3 > 0) { 
+      timer3 = timer3 - 1;
+    }
+
+    if (frameCount % 30 == 0 && timer4 > 0) { 
+      timer4 = timer4 - 1;
+    }
+
+  if(timer3 === 0 && timer4 === 0){
+    gameState = MULTIPLAYER_OVER; 
+  }
+
+  switch(score){
+    case 10: monkey.scale = 0.09;
+    break;  
+    case 20: monkey.scale = 0.1;
+    break;
+    case 30: monkey.scale = 0.11;
+    break;
+    case 40: monkey.scale = 0.12;
+    break;
+    case 50: monkey.scale = 0.13;
+    break;
+    default: break;
+}
+
+switch(score2){
+  case 10: monkey2.scale = 0.09;
+  break;  
+  case 20: monkey22.scale = 0.1;
+  break;
+  case 30: monkey2.scale = 0.11;
+  break;
+  case 40: monkey2.scale = 0.12;
+  break;
+  case 50: monkey2.scale = 0.13;
+  break;
+  default: break;
+}
+  }
+
+
+  if(gameState === MULTIPLAYER_OVER){
+    background(174,229,224);
+
+    homePage.visible = true; 
+    homeMonkey.visible = true; 
+
+    monkey.visible = false;  
+    ground2.visible = false; 
+    spaceBar.visible = false;
+    monkey2.visible = false; 
+    
+  obstacleGroup.setLifetimeEach(0);
+  bananaGroup.setLifetimeEach(0);
+
+  obstacle1Group.setLifetimeEach(0);
+  banana1Group.setLifetimeEach(0);
+
+  textSize(15);
+  textFont("Calibri");
+  stroke("black");
+  fill("black");
+  text("Player 1's Score:" + score, 150,150);
+  text("Player 2's Score:" + score2, 150,200);
+
+  if(score > score2){
+    textSize(25);
+    text("Player 1 wins!", 150,250);
+  }
+
+  if(score === score2){
+    textSize(25);
+    text("Tied Game!", 150,250);
+  }
+
+  if(score < score2){
+    textSize(25);
+    text("Player 2 wins!", 150,250);
+  }
+
+ if(keyDown("h")){
+      gameState = HOME; 
+    }
+    monkey.y = 335;
+    monkey2.y = 135; 
+  }
   if(gameState === STONEATTACKOVER){
 
     homePage.visible = false; 
@@ -676,7 +906,7 @@ if(score >= 60){
   bananaGroup.setVelocityXEach(0);
 
   obstacleGroup.setLifetimeEach(-150);
-  bananaGroup.setLifetimeEach(-150);
+  bananaGroup.setLifetimeEach(-150)
 
   if(keyDown("h")){
     obstacleGroup.visible = false;
@@ -734,7 +964,7 @@ function spawnObstacles() {
 function spawnFood() {
   if (frameCount % 135 === 0) {
     var banana = createSprite(450,220,40,10);
-    banana.y = Math.round(random(200,220));
+    banana.y = Math.round(random(220,240));
     banana.addImage(bananaImage);
     banana.scale = 0.05;
     banana.velocityX = -3;
@@ -743,3 +973,24 @@ function spawnFood() {
   }
 }
 
+function spawnObstacles2() {
+  if(frameCount % 120 === 0) {
+    var obstacle1 = createSprite(450,150,10,40);
+    obstacle1.addImage("obstacles", obstacleImage);
+    obstacle1.scale = 0.1;
+    obstacle1.velocityX = -4;
+    obstacle1Group.add(obstacle1);
+  }
+} 
+
+function spawnFood2() {
+  if (frameCount % 135 === 0) {
+    var banana1 = createSprite(450,220,40,10);
+    banana1.y = Math.round(random(20,40));
+    banana1.addImage(bananaImage);
+    banana1.scale = 0.05;
+    banana1.velocityX = -3;
+    banana1.lifetime = 200;
+     banana1Group.add(banana1);
+  }
+}
